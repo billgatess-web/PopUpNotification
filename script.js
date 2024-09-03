629d30e406bb096451abfa722660b29ef48ce099
billgatess-web <billgatess990@gmail.com>
2024-09-03 21:09:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let popup = document.getElementsByClassName("popup2")[0];
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
